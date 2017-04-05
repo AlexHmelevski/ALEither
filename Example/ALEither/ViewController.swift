90c086a9bd7ef2315d826c5024fd7ac0440b4545
Alex Hmelevski <ahmelevski.cg.ide@C02QTCMPG8WN.local>
2017-04-05 10:37:40 -0400

    Initial commit

---
+++ Example/ALEither/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ALEither
+//
+//  Created by AlexHmelevskiAG on 04/05/2017.
+//  Copyright (c) 2017 AlexHmelevskiAG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
